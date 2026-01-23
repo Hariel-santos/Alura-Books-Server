9e3f79f074583d283550d3a192954d6c52365946
Hariel Santos <harielp.santos1@gmail.com>
2026-01-23 15:27:05 -0400

    rotasFiltros

--- servicos/livro.js
+++ servicos/livro.js
@@ -15,8 +15,27 @@ function insereLivro(livroNovo){
     const novaListaDeLivros = [...livros, livroNovo];
     fs.writeFileSync('livros.json', JSON.stringify(novaListaDeLivros));
 }
+
+function modificaLivro(modificacoes,  id) {
+    let livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
+    const indiceModificado = livrosAtuais.findIndex(livro => livro.id === id)
+    
+    const conteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes }
+
+    livrosAtuais[indiceModificado] = conteudoMudado
+    fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
+}
+
+function deletarLivro(id) {
+    const livros = JSON.parse(fs.readFileSync('livros.json'));
+    const livroFiltrado = livros.filter(livro => livro.id !== id);
+    fs.writeFileSync('livros.json', JSON.stringify(livroFiltrado));
+}
+
 module.exports = {
     getTodosLivros,
     getLivroPorId,
-    insereLivro
+    insereLivro,
+    modificaLivro,
+    deletarLivro
 }
